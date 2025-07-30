e93efc598b8906f1acd38adf0068a072ade19b9d
sanjitrao16 <sanjitrao1605@gmail.com>
2025-07-30 18:34:58 +0530

    Solved Questions from 30th July

--- Binary_Search/src/Miscellaneous.java
+++ Binary_Search/src/Miscellaneous.java
@@ -4,7 +4,10 @@ public class Miscellaneous {
     public static void main(String[] args) {
         int[] arr = {3, 4, 4, 7, 8, 10};
 //        System.out.println(upperBound(arr,9));
-        System.out.println(Arrays.toString(floorCeilArray(arr,5)));
+//        System.out.println(Arrays.toString(floorCeilArray(arr,5)));
+        int[][] matrix = {{0,1,1,1},{0,0,1,1},{1,1,1,1}};
+
+        System.out.println(findMaxOnes(matrix));
     }
 
     static int lowerBound(int[] arr,int x) {
@@ -97,5 +100,36 @@ public class Miscellaneous {
         }
 
         return ans;
+    }
+
+    // Find the row with maximum number of 1's
+    static int findMaxOnes(int[][] mat) {
+        int maxOneRow = -1;
+        int maxOneCount = 0;
+        for (int i = 0;i < mat.length; i++) {
+            int start = 0;
+            int end = mat[i].length-1;
+            int oneCount = 0;
+
+            while (start <= end) {
+                int mid = start + (end-start)/2;
+
+                if (mat[i][mid] == 1) {
+                    oneCount += end-mid+1;
+                    end = mid-1;
+                }
+
+                else {
+                    start = mid+1;
+                }
+            }
+
+            if (oneCount > 0 && Math.max(oneCount,maxOneCount) == oneCount) {
+                maxOneRow = i;
+                maxOneCount = oneCount;
+            }
+        }
+
+        return maxOneRow;
     }
 }
